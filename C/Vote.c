2ec759ba4029beb5bd6cdade84df9bce9e8aec20
Sergey <thefanthomasfan@gmail.com>
2021-04-14 22:34:10 +0200

    Added some more solutions

--- C/Vote.c
+++ C/Vote.c
@@ -4,12 +4,14 @@ int main() {
     int n;
     scanf("%d", &n);
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         int m;
         scanf("%d", &m);
         int votes[m];
 
-        for (int j = 0; j < m; j++) {
+        for (int j = 0; j < m; j++)
+        {
             int v;
             scanf("%d", &v);
             votes[j] = v;
@@ -18,29 +20,38 @@ int main() {
         int winner = votes[0];
         int winnerIndex = 1;
         for (int j = 1; j < m; j++)
+        {
             if (votes[j] > winner) {
                 winner = votes[j];
                 winnerIndex = j + 1;
             }
+        }
 
         int drawcounter = 0;
         for (int j = 0; j < m; j++)
-            if (votes[j] == winner)
+        {
+            if (votes[j] == winner) {
                 drawcounter++;
+            }
+        }
 
-        if (drawcounter > 1)
+        if (drawcounter > 1) {
             printf("no winner\n");
+        }
 
         else {
             int votecounter = 0;
             for (int j = 0; j < m; j++)
+            {
                 votecounter += votes[j];
+            }
             
             int majorityChecker = votecounter / 2;
-            if (winner > majorityChecker)
+            if (winner > majorityChecker) {
                 printf("majority winner %d\n", winnerIndex);
-            else
+            } else {
                 printf("minority winner %d\n", winnerIndex);
+            }
         }
     }
 }
